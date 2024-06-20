f8d7d94a89c239b9f1366f8772d62122691b59ea
LanceHE6 <2765543491@qq.com>
2024-06-20 09:56:23 +0800

    init.init commit

---
+++ server/FangCheGateway/src/main/java/com/fangche/gateway/FangCheGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.fangche.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FangCheGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FangCheGatewayApplication.class, args);
+    }
+
+}
