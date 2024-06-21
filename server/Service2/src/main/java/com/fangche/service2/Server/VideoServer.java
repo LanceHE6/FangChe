ef488eb02fdaeaacb56945f2a80628ec8d7249f0
TangChao132 <971926434@qq.com>
2024-06-21 15:18:47 +0800

    视频类操作（增删改查），视频存放在本地

---
+++ server/Service2/src/main/java/com/fangche/service2/Server/VideoServer.java
@@ -0,0 +1,15 @@
+package com.fangche.service2.Server;
+
+
+import com.fangche.service2.Pojo.entity.Video;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.util.List;
+
+public interface VideoServer {
+    public List<Video> videoList(String name ,Long timeLength,String days);
+    public boolean videoAdd(MultipartFile video, String name);
+
+    Boolean updateById(String id, String name);
+}
