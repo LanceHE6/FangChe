ce32e3653bdd38b59650eee051930df2dbad63e0
LanceHE6 <2765543491@qq.com>
2024-06-21 09:53:18 +0800

    feat.实现更新,查询接口

--- server/Service1/src/main/java/com/fangche/service1/service/VerifyCodeService.java
+++ server/Service1/src/main/java/com/fangche/service1/service/VerifyCodeService.java
@@ -4,4 +4,6 @@ import com.fangche.service1.entity.VerifyCode;
 
 public interface VerifyCodeService {
     void insert(VerifyCode verifyCode);
+    VerifyCode selectByAccount(String account);
+    void update(VerifyCode verifyCode);
 }
