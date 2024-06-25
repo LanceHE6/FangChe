69fb053388b2e4158bf0b354cbffab902cd2e8c3
LanceHE6 <2765543491@qq.com>
2024-06-25 10:51:03 +0800

    feat.简化路由配置

--- server/Gateway/src/main/java/com/fangche/gateway/GatewayApplication.java
+++ server/Gateway/src/main/java/com/fangche/gateway/GatewayApplication.java
@@ -15,23 +15,14 @@ public class GatewayApplication {
 
     // 配置网关路由
     @Bean
-    public RouteLocator service1RouteLocator(RouteLocatorBuilder builder) {
+    public RouteLocator RouteLocator(RouteLocatorBuilder builder) {
         String service1URL = "http://127.0.0.1:8081";
-        RouteLocatorBuilder.Builder routes = builder.routes();
-        routes.route("hello1", r -> r.path("/hello1").uri(service1URL));
-        routes.route("api/user/get/{uid}", r -> r.path("/api/user/get/{uid}").uri(service1URL));
-        routes.route("api/user/register/sendCode", r -> r.path("/api/user/register/sendCode").uri(service1URL));
-        routes.route("api/user/register/verifyCode", r -> r.path("/api/user/register/verifyCode").uri(service1URL));
-        routes.route("api/user/login", r -> r.path("/api/user/login").uri(service1URL));
-
         String service2URL = "http://127.0.0.1:8082";
 
-        routes.route("api/video/list", r -> r.path("/api/video/list").uri(service2URL));
-        routes.route("api/video/add", r -> r.path("/api/video/add").uri(service2URL));
-        routes.route("api/video/put", r -> r.path("/api/video/put").uri(service2URL));
-        routes.route("api/video/delete", r -> r.path("/api/video/delete").uri(service2URL));
-        routes.route("api/video/deleteBach", r -> r.path("/api/video/deleteBach").uri(service2URL));
-        return routes.build();
+        return builder.routes()
+                .route("user_routes", r -> r.path("/api/user/**").uri(service1URL))
+                .route("video_routes", r -> r.path("/api/video/**").uri(service2URL))
+                .build();
     }
 
 }
