cb7230eb451a52959b49ba4bb6c4a0d3dcbdeb39
hkd <1934456987@qq.com>
2024-06-28 16:10:17 +0800

    前端登录

---
+++ web/src/views/main.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header style="background: white;height: 20vh;width: 30vw">Header</el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
