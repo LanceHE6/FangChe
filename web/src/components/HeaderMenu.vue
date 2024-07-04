375d674fb202d6bf3a7ba4e4521944085003b892
LanceHE6 <2765543491@qq.com>
2024-07-04 08:06:16 +0800

    feat.新增头部导航栏全局组件

---
+++ web/src/components/HeaderMenu.vue
@@ -0,0 +1,74 @@
+<script setup>
+
+import {Avatar} from "@element-plus/icons-vue";
+</script>
+
+<template>
+
+      <el-header class="menu-header">
+        <el-menu
+            :default-active="activeIndex"
+            mode="horizontal"
+            @select="handleSelect"
+            class="menu"
+            :ellipsis="false"
+        >
+
+          <img
+              class="main-logo"
+              src="../assets/logo3.png"
+              alt="Logo"
+          />
+
+
+          <el-menu-item index="1" style="font-size: 28px; margin-left: 40px">在线课程</el-menu-item>
+          <el-menu-item index="2" style="font-size: 28px; margin-left: 40px">测评</el-menu-item>
+
+          <div class="menu-right"/>
+          <el-sub-menu index="3">
+            <template #title>
+              <el-icon>
+                <el-icon :size="60"><Avatar /></el-icon>
+              </el-icon>
+              <div style="font-size: 20px">
+                {{'用户'}}
+              </div>
+
+            </template>
+            <el-menu-item index="/" >用户中心</el-menu-item>
+            <el-menu-item @click="">帮助中心</el-menu-item>
+            <el-menu-item @click="">关于</el-menu-item>
+            <el-menu-item @click="">退出登录</el-menu-item>
+          </el-sub-menu>
+
+        </el-menu>
+
+      </el-header>
+
+</template>
+
+<style scoped>
+* {
+  padding: 0;
+  margin: 0;
+}
+
+.menu-header{
+  position: fixed;
+  width: 100%;
+
+}
+.menu {
+  height: 72px;
+}
+.main-logo {
+  width: 150px;
+  height: 100px;
+  margin-top: -5px;
+  margin-left: 5%;
+}
+
+.menu-right {
+  flex-grow: 1;
+}
+</style>
