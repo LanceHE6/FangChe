1c4a2225c13382237152c271ab7055dd1ba557e8
LanceHE6 <2765543491@qq.com>
2024-07-11 08:18:04 +0800

    fix.修复视频searchById的请求方法错误

--- server/Service2/src/main/java/com/fangche/service2/controller/VideoController.java
+++ server/Service2/src/main/java/com/fangche/service2/controller/VideoController.java
@@ -1,6 +1,8 @@
 package com.fangche.service2.controller;
 
 
+//import com.fangche.service1.utils.authority.Authority;
+//import com.fangche.service1.utils.authority.Permission;
 
 import com.fangche.service2.Pojo.dto.Result;
 import com.fangche.service2.Server.Imp.VideoServerImp;
@@ -20,13 +22,14 @@ public class VideoController {
 
     //videoList是根据条件查询，如：发布时间查询，视频名称查询，视频时长
     @GetMapping("/list")
+//    @Authority
     public Result videoList(@RequestParam(value = "name",required = false) String name,
                             @RequestParam(value = "timeLength", defaultValue = "-1") Long timeLength,
                             @RequestParam(value = "days",required = false) String days) {
         return videoServer.videoList(name, timeLength, days);
     }
 
-    @PostMapping("/search")
+    @GetMapping("/search")
     public Result searchById(@RequestParam("id") Long id){
         return videoServer.selectById(id);
     }
@@ -38,21 +41,25 @@ public class VideoController {
 
 
     @PostMapping("/add")
+//    @Authority(Permission.AUTHOR)
     public Result videoAdd(@RequestParam("video") MultipartFile video, @RequestParam("name") String name) throws IOException {
         return videoServer.videoAdd(video, name);
     }
 
     @PutMapping("/put")
+//    @Authority(Permission.AUTHOR)
     public Result videoAlterById(@RequestParam("id") Long id, @RequestParam("name") String name) {
         return  videoServer.updateById(id, name);
     }
 
     @DeleteMapping("/delete")
+//    @Authority(Permission.AUTHOR)
     public Result deleteById(Long id) {
         return videoServer.deleteById(id);
     }
 
     @DeleteMapping("/deleteBach")
+//    @Authority(Permission.AUTHOR)
     public Result deleteBach(ArrayList<Integer> list) {
         return videoServer.deleteBatchIds(list);
     }
